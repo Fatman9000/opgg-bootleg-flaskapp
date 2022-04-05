bb51428e3207cac3f1759ea39abbf10b1e89508b
fatman9000 <conman0503@gmail.com>
2022-04-05 17:06:43 -0400

    cleaned up functions and added docstrings

--- League_opgg_bootleg.py
+++ League_opgg_bootleg.py
@@ -1,11 +1,8 @@
 import json
 import os
-from datetime import datetime
 import requests as r
 from pymongo import MongoClient
 import re
-from pprint import pprint
-from database import Database
 
 
 with open("./config/config.json") as file:
@@ -14,26 +11,63 @@ client = MongoClient(
     f"mongodb://{configsettings['host']}:{configsettings['port']}")
 db = client.leagueData
 # serverStatusResult=db.command("serverStatus")
+try:
+    api_key = os.environ.get("RIOT_ENV_VAR")
+except FileNotFoundError:
+    print("No api key file found")
+    exit()
+# current_date = datetime.today().strftime("%Y-%m-%d_%H-%M")
+headers = {"Content-Type": "application/json",
+            "Application-Type": "application/json", "X-Riot-Token": api_key}
 
 
 def pull_user_data(league_name, update_info):
+    """
+    pull_user_data sets the mongo database client and queries the riot games api
+    in order to build and return a dictionary of player information while also updating
+    stored info in the playerData collection.
+    """
     client = MongoClient("mongodb://localhost:27017")
     db = client.leagueData
     existing_player_info = db.playerData
     player_in_database = existing_player_info.find_one(
         {'name': re.compile('^' + re.escape(league_name) + '$', re.IGNORECASE)})
+    
     if update_info == False:
         if player_in_database:
             return player_in_database
         # print(player_in_database)
+
+    user_data = get_summoner(league_name)
+    summoner_entries = get_summoner_entries(user_data)
+    match_history = get_summoner_history(user_data)
+
     try:
-        api_key = os.environ.get("RIOT_ENV_VAR")
-    except FileNotFoundError:
-        print("No api key file found")
-        exit()
-    # current_date = datetime.today().strftime("%Y-%m-%d_%H-%M")
-    headers = {"Content-Type": "application/json",
-               "Application-Type": "application/json", "X-Riot-Token": api_key}
+        for match_id in player_in_database["matchIds"]:
+            if match_id not in match_history:
+                match_history.append(match_id)
+    except:
+        pass
+
+    player_info = {
+        "name": league_name,
+        "userData": [user_data.json(), ],
+        "summonerEntries": summoner_entries,
+        "matchIds": match_history
+    }
+
+    if update_info == True:
+        update_info(player_info)
+    else:
+        db.playerData.insert_one(player_info)
+    client.close()
+    return player_info
+
+
+def get_summoner(league_name):
+    """
+    Takes the summoner's name to query the riot games api for basic account info.
+    """
     url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}".format(
         league_name)
     try:
@@ -42,62 +76,46 @@ def pull_user_data(league_name, update_info):
         return {"status": "something really went wrong"}
     if user_data.status_code == 404:
         return {"status": "user not found"}
+    return user_data
 
+
+def get_summoner_entries(user_data):
+    """
+    Takes the account information returned by get_summoner to query the riot games api for
+    the user's, leagueId, summonerId, wins and losses, and rank info.
+    """
     try:
         summoner_entries = r.get("https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/{}".format(
             user_data.json()["id"]), headers=headers).json()  # end_index, start_index),
     except Exception as e:
         print("failed to pull user data: error: {}".format(e))
+    return summoner_entries
 
+
+def get_summoner_history(user_data):
     try:
         match_history = r.get("https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{}/ids?start=0&count=20".format(
             user_data.json()["puuid"]), headers=headers).json()
     except Exception as e:
         print("failed to pull user data: error: {}".format(e))
+    return match_history
 
-    # game_ids = [{str(x["gameId"]): x["timestamp"]}
-    #             for x in match_history["matches"]]
 
-    # temp_dict = {}
-    # for my_dict in game_ids:
-    #     temp_dict.update(my_dict)
-
-    # json_of_match_ids = return_match_ids(league_name)
-    # match_ids = {}
-    # for x in game_ids:
-    #     match_ids.update(x)
-    try:
-        for match_id in player_in_database["matchIds"]:
-            if match_id not in match_history:
-                match_history.append(match_id)
-    except:
-        pass
-    player_info = {
-
-        "name": league_name,
-        "userData": [user_data.json(), ],
-        "summonerEntries": summoner_entries,
-        # "matchHistory": match_history["matches"],
-        # "indices": [{
-        #     "startIndex": match_history["startIndex"], "endIndex": match_history["endIndex"],
-        #     "totalGames": match_history["totalGames"]
-        # }],
-        "matchIds": match_history
-
-    }
-    if update_info == True:
-        db.playerData.update_one({'name': re.compile('^' + re.escape(league_name) + '$', re.IGNORECASE)}, {'$set': {
-                                 'userData': player_info['userData'], 'summonerEntries': player_info['summonerEntries']}})
-        for match_id in player_info['matchIds']:
-            db.playerData.update_one({'name': re.compile(
-                '^' + re.escape(league_name) + '$', re.IGNORECASE)}, {'$addToSet': {'matchIds': match_id}})
-    else:
-        db.playerData.insert_one(player_info)
-    client.close()
-    return player_info
+def update_info(player_info):
+    """
+    Updates existing player info in the database with provided player info
+    """
+    db.playerData.update_one({'name': re.compile('^' + re.escape(league_name) + '$', re.IGNORECASE)}, {'$set': {
+        'userData': player_info['userData'], 'summonerEntries': player_info['summonerEntries']}})
+    for match_id in player_info['matchIds']:
+        db.playerData.update_one({'name': re.compile(
+            '^' + re.escape(league_name) + '$', re.IGNORECASE)}, {'$addToSet': {'matchIds': match_id}})
 
 
 def display_match(match_id=None, league_name=None):
+    """
+    Pulls existing match info for the provided match_id from the database and returns it.
+    """
     try:
         api_key = os.environ.get("RIOT_ENV_VAR")
     except FileNotFoundError:
@@ -123,6 +141,9 @@ def display_match(match_id=None, league_name=None):
 
 
 def db_patch_data(patch):
+    """
+    Updates the database with new information when the games is patched
+    """
     db.patchData.delete_many({})
     db.runeData.delete_many({})
     with open(f"./static/{patch}/data/en_US/item.json") as f:
@@ -140,6 +161,9 @@ def db_patch_data(patch):
 
 
 def get_item_info(match_id):
+    """
+    Pulls item info for a given match from the database and returns it
+    """
     existing_match_info = db.matchData.find_one({"_id": match_id}, {"info.participants.item0": 1, "info.participants.item1": 1, "info.participants.item2": 1,
                                                 "info.participants.item3": 1, "info.participants.item4": 1, "info.participants.item5": 1, "info.participants.item6": 1})
     player_num = 0
@@ -173,11 +197,6 @@ if __name__ == "__main__":
     if player_in_database is None:
         print("Info Not in database")
         pud = pull_user_data(league_name)
-
-
-# db.playerData.update_one({'_id}, {player_})
-#
-    # match_id = input("input match id ")
 
 
 #this comment is for testing
