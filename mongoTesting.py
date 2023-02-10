7b8c34ae5d504a76381e35a7d4912728ec499da7
Fatman9000 <conman0503@gmail.com>
2023-02-10 13:06:11 -0500

    Cleaning up

--- mongoTesting.py
+++ mongoTesting.py
@@ -2,9 +2,11 @@ from pymongo import MongoClient
 from pprint import pprint
 
 client = MongoClient("mongodb://localhost:27017")
-db=client.leagueData
+db = client.leagueData
 
 
-summonerEntries = db.playerData.find_one({"name": "Fatman9000"}, {"summonerEntries": 1})["summonerEntries"][0]
+summonerEntries = db.playerData.find_one(
+    {"name": "Fatman9000"}, {"summonerEntries": 1}
+)["summonerEntries"][0]
 
 print(summonerEntries["tier"] + summonerEntries["rank"])
--- player.py
+++
@@ -1,5 +0,0 @@
-class player():
-    def __init__(self, league_name, client) -> None:
-        self.league_name = league_name
-        self.client = client
-
